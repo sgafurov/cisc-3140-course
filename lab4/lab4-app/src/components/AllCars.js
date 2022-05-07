16c19b464dc5d47a587003c3d73484f25fdaf65a
Shakhram <shakag6@gmail.com>
2022-05-07 18:39:57 -0400

    Display data in a table format. Create styles sheet for table design

--- lab4/lab4-app/src/components/AllCars.js
+++ lab4/lab4-app/src/components/AllCars.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import '../styles/TableStyle.css'
 
 export default function AllCars(props) {
     const [cars, setCars] = useState([])
@@ -30,18 +31,91 @@ export default function AllCars(props) {
 
             <button onClick={fetchData}>Get all cars data</button>
 
-            <tr>Timestamp,Email,Name,Year,Make,Model,Car_ID,Judge_ID,Judge_Name,Racer_Turbo,Racer_Supercharged,Racer_Performance,Racer_Horsepower,Car_Overall,Engine_Modifications,Engine_Performance,Engine_Chrome,Engine_Detailing,Engine_Cleanliness,Body_Frame_Undercarriage,Body_Frame_Suspension,Body_Frame_Chrome,Body_Frame_Detailing,Body_Frame_Cleanliness,Mods_Paint,Mods_Body,Mods_Wrap,Mods_Rims,Mods_Interior,Mods_Other,Mods_ICE,Mods_Aftermarket,Mods_WIP,Mods_Overall</tr>
-            {cars.map((car, index) => {
-                return (
-                    <>
-                        <tr>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Timestamp</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Year</th>
+                        <th>Make</th>
+                        <th>Model</th>
+                        <th>Car_ID</th>
+                        <th>Judge_ID</th>
+                        <th>Judge_Name</th>
+                        <th>Racer_Turbo</th>
+                        <th>Racer_Supercharged</th>
+                        <th>Racer_Performance</th>
+                        <th>Racer_Horsepower</th>
+                        <th>Car_Overall</th>
+                        <th>Engine_Modifications</th>
+                        <th>Engine_Performance</th>
+                        <th>Engine_Chrome</th>
+                        <th>Engine_Detailing</th>
+                        <th>Engine_Cleanliness</th>
+                        <th>Body_Frame_Undercarriage</th>
+                        <th>Body_Frame_Suspension</th>
+                        <th>Body_Frame_Chrome</th>
+                        <th>Body_Frame_Detailing</th>
+                        <th>Body_Frame_Cleanliness</th>
+                        <th>Mods_Paint</th>
+                        <th>Mods_Body</th>
+                        <th>Mods_Wrap</th>
+                        <th>Mods_Rims</th>
+                        <th>Mods_Interior</th>
+                        <th>Mods_Other</th>
+                        <th>Mods_ICE</th>
+                        <th>Mods_Aftermarket</th>
+                        <th>Mods_WIP</th>
+                        <th>Mods_Overall</th>
+                    </tr>
+                </thead>
 
-                            <tr>{cars[index].Timestamp}, {cars[index].Email}, {cars[index].Name}, {cars[index].Year}, {cars[index].Make}, {cars[index].Model}, {cars[index].Car_ID}, {cars[index].Judge_ID},{cars[index].Judge_Name}, {cars[index].Racer_Turbo}, {cars[index].Racer_Supercharged}, {cars[index].Racer_Performance}, {cars[index].Racer_Horsepower}, {cars[index].Car_Overall}, {cars[index].Engine_Modifications}, {cars[index].Engine_Performance}, {cars[index].Engine_Chrome}, {cars[index].Engine_Detailing}, {cars[index].Engine_Cleanliness}, {cars[index].Body_Frame_Undercarriage}, {cars[index].Body_Frame_Suspension}, {cars[index].Body_Frame_Chrome}, {cars[index].Body_Frame_Detailing}, {cars[index].Body_Frame_Cleanliness}, {cars[index].Mods_Paint}, {cars[index].Mods_Body}, {cars[index].Mods_Wrap}, {cars[index].Mods_Rims}, {cars[index].Mods_Interior}, {cars[index].Mods_Other}, {cars[index].Mods_ICE}, {cars[index].Mods_Aftermarket}, {cars[index].Mods_WIP}, {cars[index].Mods_Overall}</tr>
-                        </tr>
-                    </>
-                );
-            })}
-
+                <tbody>
+                    {cars.map((car, index) => {
+                        return (
+                            <>
+                                <tr>
+                                    <td>{cars[index].Timestamp}</td>
+                                    <td>{cars[index].Email}</td>
+                                    <td>{cars[index].Name}</td>
+                                    <td>{cars[index].Year}</td>
+                                    <td>{cars[index].Make}</td>
+                                    <td>{cars[index].Model}</td>
+                                    <td>{cars[index].Car_ID}</td>
+                                    <td>{cars[index].Judge_ID}</td>
+                                    <td>{cars[index].Judge_Name}</td>
+                                    <td>{cars[index].Racer_Turbo}</td>
+                                    <td>{cars[index].Racer_Supercharged}</td>
+                                    <td>{cars[index].Racer_Performance}</td>
+                                    <td>{cars[index].Racer_Horsepower}</td>
+                                    <td>{cars[index].Car_Overall}</td>
+                                    <td>{cars[index].Engine_Modifications}</td>
+                                    <td>{cars[index].Engine_Performance}</td>
+                                    <td>{cars[index].Engine_Chrome}</td>
+                                    <td>{cars[index].Engine_Detailing}</td>
+                                    <td>{cars[index].Engine_Cleanliness}</td>
+                                    <td>{cars[index].Body_Frame_Undercarriage}</td>
+                                    <td>{cars[index].Body_Frame_Suspension}</td>
+                                    <td>{cars[index].Body_Frame_Chrome}</td>
+                                    <td>{cars[index].Body_Frame_Detailing}</td>
+                                    <td>{cars[index].Body_Frame_Cleanliness}</td>
+                                    <td>{cars[index].Mods_Paint}</td>
+                                    <td>{cars[index].Mods_Body}</td>
+                                    <td>{cars[index].Mods_Wrap}</td>
+                                    <td>{cars[index].Mods_Rims}</td>
+                                    <td>{cars[index].Mods_Interior}</td>
+                                    <td>{cars[index].Mods_Other}</td>
+                                    <td>{cars[index].Mods_ICE}</td>
+                                    <td>{cars[index].Mods_Aftermarket}</td>
+                                    <td>{cars[index].Mods_WIP}</td>
+                                    <td>{cars[index].Mods_Overall}</td>
+                                </tr>
+                            </>
+                        );
+                    })}
+                </tbody>
+            </table>
         </>
     )
 }
